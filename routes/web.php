f9f4b4a59818545569fac3ca7f3219a132374aaa
Lukman Sanjaya <lukman.rocks@live.com>
2020-07-17 10:26:46 +0700

    routing view

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('user', function(){
+    $data = array(
+        'name'=> 'Lukman'
+    );
+    return view('user', $data);
 });
